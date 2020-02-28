ead24189ccbf8339ba4c2e3513432bc0f6232059
Varun Chodha <60072034+varunvc010@users.noreply.github.com>
2020-02-28 17:48:05 +0530

    Add files via upload

---
+++ Binary_Coders.ino
@@ -0,0 +1,60 @@
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial mySerial(4, 5);
+
+int trigPin = 6;
+int echoPin = 7;
+int led=12;
+
+void setup() {
+   mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+   
+  
+   pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led,OUTPUT);
+  delay(100);
+  
+  
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  delay(1);
+ 
+ if(distance<=20){
+SendMessage();
+Serial.write("Sending Alert ");
+ digitalWrite(led,HIGH);
+ delay(1000);
+}
+else
+{
+ digitalWrite(led,LOW);
+}}
+ void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+918572039226\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("Landslide at Wakna003, Reach immediately.");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+
+ void RecieveMessage()
+{
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to receive a live SMS
+  delay(1000);
+ }
